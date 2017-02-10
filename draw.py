bc3d309c2b1e99e5eed6299ac0d47ea63da0204e
xinhuixu <x.xu.1102@gmail.com>
2017-02-10 15:28:31 -0500

    add my algo lmao

--- draw.py
+++ draw.py
@@ -3,6 +3,17 @@ from display import *
 def draw_line( screen, x0, y0, x1, y1, color ):
     x = x0
     y = y0
+    m = (y1 - y0) / (1.0 * (x1 - x0))
+    b = y - m * x 
+    while x < x1:
+        plot( screen, color, x, len(screen) - y )
+        x += 1 
+        y = int(m * x + b) #dont work bc non integer indices
+        
+        
+def bresenham_draw_line( screen, x0, y0, x1, y1, color ):
+    x = x0
+    y = y0
     A = y1 - y0 #A = delta y
     B = -(x1 - x0) #B = -delta x
     d = 2 * A + B
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from display import *
 from draw import *
 
-screen = new_screen()
-color = [ 0, 255, 0 ]
+def main(x0, y0, x1, y1, color, algo):
+    screen = new_screen()
+    #color = [ 0, 255, 0 ]
+    if algo == 'b':
+        bresenham_draw_line(screen, x0, y0, x1, y1, color)
+    elif algo == 'm':
+        draw_line(screen, x0, y0, x1, y1, color)
+    else:
+        print 'error'    
+    display(screen)
+    save_extension(screen, 'img.png')
 
-draw_line(screen, 0, 0, 25, 25, color)
-display(screen)
-save_extension(screen, 'img.png')
+main(0,0,400,200,[0,255,0],'b')
+main(0,0,400,200,[255,0,0],'m')
