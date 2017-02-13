783b4999483d01984172ffa6e26c1be08e4f6a90
Xinhui <x.xu.1102@gmail.com>
2017-02-13 15:24:03 -0500

    oct8

--- draw.py
+++ draw.py
@@ -23,5 +23,22 @@ def bresenham_draw_line( screen, x0, y0, x1, y1, color ):
         if d > 0:
             y += 1
             d += 2 * B
+            
+        x += 1
+        d += 2 * A
+
+def eight_bresenham_draw_line( screen, x0, y0, x1, y1, color ):
+    x = x0
+    y = y0
+    A = y1 - y0 #A = delta y
+    B = -(x1 - x0) #B = -delta x
+    d = 2 * A - B
+
+    while x < x1:
+        plot( screen, color, x, len(screen) - y)
+        if d < 0:
+            y -= 1
+            d -= 2 * B
+            
         x += 1
         d += 2 * A
