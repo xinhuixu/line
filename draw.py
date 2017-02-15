a55b489848f90d715f3931e9e142c8fea0fb0b8f
Xinhui <x.xu.1102@gmail.com>
2017-02-15 15:28:05 -0500

    done

--- draw.py
+++ draw.py
@@ -12,56 +12,62 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         x += 1 
         y = int(m * x + b)
 '''        
-        
+
 def draw_line(screen, x0, y0, x1, y1, color):
+    if x0 > x1:
+        print "SWAP"
+        tx = x1
+        x1 = x0
+        x0 = tx
+        ty = y1
+        y1 = y0
+        y0 = ty
+        
     dx = x1 - x0
     dy = y1 - y0
-    m = abs(dy / dx)
+    if dx == 0:
+        m = 'und'
+    else:
+        m = abs(dy / dx)
     A = dy
     B = -1*dx
+
     print dy, dx, m, A, B
     
-    if (dy > 0) and (dx > 0): #QUADRANT I
+    if m == 'und':
+        draw_line_und(screen, x0, y0, x1, y1, color);
+    elif (dy >= 0) and (dx >= 0): #QUADRANT I
         if m <= 1: #OCTANT I    
             draw_line_I(screen, x0, y0, x1, y1, color, A, B);
         else: #OCTANT II
             draw_line_II(screen, x0, y0, x1, y1, color, A, B);
     elif (dy > 0) and (dx < 0): #QUADRANT II
         if m <= 1: #OCTANT IV
-            draw_line_IV(screen, x0, y0, x1, y1, color, A, B);
+            draw_line_VIII(screen, x0, y0, x1, y1, color, A, B);
         else: #OCTANT III
-            draw_line_III(screen, x0, y0, x1, y1, color, A, B);
-    elif (dy < 0) and (dx < 0): #QUADRANT III
+            draw_line_VII(screen, x0, y0, x1, y1, color, A, B);
+    elif (dy <= 0) and (dx <= 0): #QUADRANT III
         if m <= 1: #OCTANT V
-            draw_line_V(screen, x0, y0, x1, y1, color, A, B);
+            draw_line_I(screen, x0, y0, x1, y1, color, A, B);
         else: #OCTANT VI
-            draw_line_VI(screen, x0, y0, x1, y1, color, A, B);
+            draw_line_II(screen, x0, y0, x1, y1, color, A, B);
     elif (dy < 0) and (dx > 0):
         if m <= 1: #OCTANT VIII
             draw_line_VIII(screen, x0, y0, x1, y1, color, A, B);
         else: #OCTANT VII
             draw_line_VII(screen, x0, y0, x1, y1, color, A, B);
     
-    else:
-        draw_line_quadrantal(screen, x0, y0, x1, y1, color, A, B);
-'''
-II:
-d=A+2B
-while y < y1
-   d<0
-   x++
-   d+=2A
-y++
-d+=2B
+def draw_line_und( screen, x0, y0, x1, y1, color ):
+    y = y0
+    if y0 < y1:
+        while y < y1:
+            plot(screen, color, x0, len(screen)-y)
+            y += 1
+    elif y0 > y1:
+        while y > y1:
+            plot(screen, color, x0, len(screen)-y)
+            y -= 1
 
-VII:
-
-V:
-like I but swapped
-if x1 < x0 SWAP y1 < y0 SWAP
-'''
-def draw_line_quadrantal(screen, x0, y0, x1, y1, color, A, B):
-    pass
 
 def draw_line_I( screen, x0, y0, x1, y1, color, A, B):
     x = x0
@@ -87,18 +93,6 @@ def draw_line_II( screen, x0, y0, x1, y1, color, A, B):
         y += 1
         d += 2 * B    
 
-def draw_line_III( screen, x0, y0, x1, y1, color, A, B):
-    pass
-
-def draw_line_IV( screen, x0, y0, x1, y1, color, A, B):
-    pass
-
-def draw_line_V( screen, x0, y0, x1, y1, color, A, B):
-    pass
-
-def draw_line_VI( screen, x0, y0, x1, y1, color, A, B):
-    pass
-
 def draw_line_VII( screen, x0, y0, x1, y1, color, A, B):
     x = x0
     y = y0
@@ -106,7 +100,7 @@ def draw_line_VII( screen, x0, y0, x1, y1, color, A, B):
 
     while y > y1:
         plot( screen, color, x, len(screen) - y)
-        if d < 0:
+        if d > 0:
             x += 1
             d += 2*A            
         y -= 1
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from display import *
 from draw import *
 
-screen = new_screen()
+s = new_screen()
 
-draw_line(screen,250,250,500,350,[255,153,153]) #I
-draw_line(screen,250,250,350,500,[255,204,153]) #II
-draw_line(screen,250,250,350,500,[255,204,153]) #III
-draw_line(screen,250,250,350,0,[255,204,153]) #VII
-draw_line(screen,250,250,500,150,[255,204,153]) #VIII
+org = 250
+right = top = 500
+left = bottom = 0
+
+draw_line(s,left,bottom,right,top,[0,255,0])
+draw_line(s,left,top,right,bottom,[0,255,0])
+draw_line(s,org,top,org,bottom,[0,0,255])
+draw_line(s,left,org,right,org,[0,0,255])
+print '------------------'
+
+draw_line(s,org,org,right,org+100,[255,204,153]) #I
+draw_line(s,org,org,org+100,top,[255,204,153]) #II
+draw_line(s,org,org,org-100,top,[255,204,153]) #III
+draw_line(s,org,org,left,org+100,[255,204,153]) #IV
+draw_line(s,org,org,left,org-100,[255,204,153]) #V
+draw_line(s,org,org,org-100,bottom,[255,204,153]) #VI
+draw_line(s,org,org,org+100,bottom,[255,204,153]) #VII
+draw_line(s,org,org,right,org-100,[255,204,153]) #VIII
 
 
-display(screen)
-save_extension(screen, 'img.png')
+display(s)
+save_extension(s, 'img.png')
+print "!image saved as img.png"
